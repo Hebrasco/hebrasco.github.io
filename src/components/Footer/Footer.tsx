70c7462edc8dd0817fdb43e24d66b4d4395e6566
Daniel Bedrich <hebrasco@icloud.com>
2022-03-20 17:12:40 +0100

    Fix footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Container from 'react-bootstrap/esm/Container'
 import { HashLink as Link } from 'react-router-hash-link'
 import { ROUTES } from 'constants/Routes'
+import { Col, Row } from 'react-bootstrap'
 
 import './Footer.css'
 
@@ -12,22 +13,33 @@ export default function Footer(): JSX.Element {
 
   return (
     <Container fluid className="footer bg-light text-muted">
-      <footer className="d-flex justify-content-between py-3">
-        <p className="nav-link align-items-center m-0">
-          Copyright © {getCurrentYear()} Daniel Bedrich. All rights reserved.
-        </p>
-        <div className="d-flex">
-          <Link to={ROUTES.imprintHash} smooth className="nav-link text-muted">
-            Imprint
-          </Link>
-          <Link
-            to={ROUTES.privacyPolicyHash}
-            smooth
-            className="nav-link text-muted"
-          >
-            Privacy Policy
-          </Link>
-        </div>
+      <footer className="py-3">
+        <Row>
+          <Col>
+            <p className="nav-link align-items-center m-0">
+              Copyright © {getCurrentYear()} Daniel Bedrich. All rights
+              reserved.
+            </p>
+          </Col>
+          <Col xs="12" md="auto">
+            <div className="d-flex">
+              <Link
+                to={ROUTES.imprintHash}
+                smooth
+                className="nav-link text-muted"
+              >
+                Imprint
+              </Link>
+              <Link
+                to={ROUTES.privacyPolicyHash}
+                smooth
+                className="nav-link text-muted"
+              >
+                Privacy Policy
+              </Link>
+            </div>
+          </Col>
+        </Row>
       </footer>
     </Container>
   )
