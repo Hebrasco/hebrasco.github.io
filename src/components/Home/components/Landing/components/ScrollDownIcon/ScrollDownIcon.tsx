9dc73ecf6be83ff3f285afb0d8dd376e65e1cee9
Daniel Bedrich <hebrasco@icloud.com>
2022-04-06 12:41:58 +0200

    Add click to scroller

--- src/components/Home/components/Landing/components/ScrollDownIcon/ScrollDownIcon.tsx
+++ src/components/Home/components/Landing/components/ScrollDownIcon/ScrollDownIcon.tsx
@@ -1,11 +1,15 @@
 import './ScrollDownIcon.css'
 
 import React from 'react'
+import { HashLink } from 'react-router-hash-link'
+import { ROUTES } from 'constants/routes'
 
 export default function ScrollDownIcon() {
   return (
     <div className="scroll-down-icon-container">
-      <div className="scroll-down-icon"></div>
+      <HashLink to={ROUTES.aboutMe}>
+        <div className="scroll-down-icon"></div>
+      </HashLink>
     </div>
   )
 }
