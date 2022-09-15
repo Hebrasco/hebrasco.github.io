e945deb57eca98fb3c5dbe57d73039d773b730cd
Daniel Bedrich <hebrasco@icloud.com>
2022-09-15 14:50:56 +0200

    Revert to HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { init } from '@emailjs/browser'
 import { Footer, NavBar } from 'components/layout'
 import { CONFIG } from 'data'
 import React, { useEffect } from 'react'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { HashRouter as Router } from 'react-router-dom'
 import { Routes } from 'routes'
 
 export function App() {
