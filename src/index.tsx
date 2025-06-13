353cd24e47beddbf6863f792bfb2497c010410ca
Daniel Bedrich <hebrasco@icloud.com>
2025-06-13 22:33:44 +0200

    ✨ Add new font

--- src/index.tsx
+++ src/index.tsx
@@ -6,9 +6,10 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 
 import 'assets/styles/bootstrap-overrides.css'
 import 'assets/styles/default-overrides.css'
-
 import 'assets/styles/dark-mode-overrides.css'
 
+import 'assets/styles/custom.css'
+
 const container = document.getElementById('root')
-const root = createRoot(container!) // createRoot(container!) if you use TypeScript
+const root = createRoot(container!)
 root.render(<App />)
